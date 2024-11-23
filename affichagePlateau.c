5f1449382bcbbc325fcac7654d148cd3fc3f5aed
julie znaor <julie.znaor@edu.ece.fr>
2024-11-23 15:41:42 +0100

    Update affichagePlateau.c

--- affichagePlateau.c
+++ affichagePlateau.c
@@ -1,9 +1,20 @@
-#define CONSOLE_WIDTH 120
 #include <stdio.h>
-#include <stdlib.h>
+#include <windows.h>
+#include "plateau.h"
 
+void afficherPlateau(char plateau[CASE][CASE]) {
+    short tab[9] = {'A','B','C','D','E','F','G','H','I'};
+    printf("   ");
+    for (int i = 0; i < 9; i++) printf(" %c  ", tab[i]);
+    printf("\n");
+    for (int i = 0; i < 9; i++) {
+        printf("  +---+---+---+---+---+---+---+---+---+\n");
+        printf("%d ", i + 1); // Numéro des lignes
+        for (int j = 0; j < 9; j++) {
+            printf("| %c ", plateau[i][j]);
+        }
+        printf("|\n");
+    }
+    printf("  +---+---+---+---+---+---+---+---+---+\n");
+}
 
-
-int NouvellePartie() {
-    system("cls");
-float
