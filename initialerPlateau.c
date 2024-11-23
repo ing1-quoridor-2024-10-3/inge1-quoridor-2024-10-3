0a51dd778ac6f4e41ed18dff73696f2110e2e5eb
julie znaor <julie.znaor@edu.ece.fr>
2024-11-23 15:11:29 +0100

    Update initialerPlateau.c

--- initialerPlateau.c
+++ initialerPlateau.c
@@ -1,3 +1,20 @@
-//
-// Created by joekh on 23/11/2024.
-//
+#include <stdio.h>
+#include <windows.h>
+#include "plateau.h"
+
+void afficherPlateau(char plateau[CASE][CASE]) {
+    short tab[9] = {'A','B','C','D','E','F','G','H','I'};
+    printf("   ");
+    for (int i = 0; i < 9; i++) printf(" %c  ", tab[i]);
+    printf("\n");
+    for (int i = 0; i < 9; i++) {
+        printf("  +---+---+---+---+---+---+---+---+---+\n");
+        printf("%d ", i + 1); // Numéro des lignes
+        for (int j = 0; j < 9; j++) {
+            printf("| %c ", plateau[i][j]);
+        }
+        printf("|\n");
+    }
+    printf("  +---+---+---+---+---+---+---+---+---+\n");
+}
+
