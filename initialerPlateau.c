f9dc03eac23ba4874965672993e1678571469317
julie znaor <julie.znaor@edu.ece.fr>
2024-11-23 15:43:08 +0100

    Update initialerPlateau.c

--- initialerPlateau.c
+++ initialerPlateau.c
@@ -1,3 +1,14 @@
-//
-// Created by joekh on 23/11/2024.
-//
+#include <stdio.h>
+#include "plateau.h"
+#define CASE 9
+void initialiserPlateau(char plateau[CASE][CASE]) {
+    int i=0;
+    while (i<=CASE){
+        int j=0;
+        while (j<=CASE) {
+            j++;
+            plateau[i][j] = ' ';
+        }
+        i++;
+    }
+}
