8791f09a6c253e980a9fbbf3351bed78f0da1da7
julie znaor <julie.znaor@edu.ece.fr>
2024-11-23 15:12:44 +0100

    Update initialerPlateau.c

--- initialerPlateau.c
+++ initialerPlateau.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-#include <windows.h>
 #include "plateau.h"
-
-void afficherPlateau(char plateau[CASE][CASE]) {
-    short tab[9] = {'A','B','C','D','E','F','G','H','I'};
-    printf("   ");
-    for (int i = 0; i < 9; i++) printf(" %c  ", tab[i]);
-    printf("\n");
-    for (int i = 0; i < 9; i++) {
-        printf("  +---+---+---+---+---+---+---+---+---+\n");
-        printf("%d ", i + 1); // Numéro des lignes
-        for (int j = 0; j < 9; j++) {
-            printf("| %c ", plateau[i][j]);
+#define CASE 9
+void initialiserPlateau(char plateau[CASE][CASE]) {
+    int i=0;
+    while (i<=CASE){
+        int j=0;
+        while (j<=CASE) {
+            j++;
+            plateau[i][j] = ' ';
         }
-        printf("|\n");
+        i++;
     }
-    printf("  +---+---+---+---+---+---+---+---+---+\n");
 }
-
